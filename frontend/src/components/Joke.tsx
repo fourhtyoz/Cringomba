91c7f65bfc6ce578805c47bb816deb32d868b28d
fourhtyoz <fourhtyoz@gmail.com>
2024-04-06 18:58:27 +0300

    httpserver -> axios

--- frontend/src/components/Joke.tsx
+++ frontend/src/components/Joke.tsx
@@ -6,7 +6,6 @@ import ActionBar from './ActionBar';
 import { httpServer } from '../api/httpServer';
 import { useSelector } from 'react-redux';
 import { selectMode } from '../stores/selectors/selectors';
-import axios from 'axios';
 
 
 export default function Joke() {
@@ -15,7 +14,7 @@ export default function Joke() {
     const darkMode = mode === 'dark'
     const handleClick = async () => {
         try {
-            const res: any = await axios.get('http://localhost:8000/')
+            const res: any = await httpServer.get('/')
             if (res.status === 200) {
                 setJokeText(res.data.randomJoke[0].text)
                 const hash = localStorage.getItem('cringombaGeneratedJoke')
