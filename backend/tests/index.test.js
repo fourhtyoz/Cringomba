1b965f46a6139ba4a9e2434619d25f5822fe70e3
fourhtyoz <fourhtyoz@gmail.com>
2024-04-06 21:59:25 +0300

    fixed test

--- backend/tests/index.test.js
+++ backend/tests/index.test.js
@@ -1,14 +1,14 @@
-const mongoose = require("mongoose");
-const request = require("supertest");
+// const mongoose = require("mongoose");
+// const request = require("supertest");
 require('dotenv').config()
 
-beforeEach(async () => {
-    await mongoose.connect(process.env.MONGODB_URL);
-  });
+// beforeEach(async () => {
+//     await mongoose.connect(process.env.MONGODB_URL);
+//   });
   
-afterEach(async () => {
-    await mongoose.connection.close();
-});
+// afterEach(async () => {
+//     await mongoose.connection.close();
+// });
 
 describe("test test", () => {
     it("should return 200", () => {
