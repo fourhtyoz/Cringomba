da7258158362e495803a9815a0dc99803c6ebc57
fourhtyoz <fourhtyoz@gmail.com>
2024-04-07 15:25:19 +0300

    localhost -> https://backend-ejlb.onrender.com

--- frontend/src/api/httpServer.js
+++ frontend/src/api/httpServer.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const httpServer = axios.create({
-    baseURL: 'http://localhost:8000'
+    baseURL: 'https://backend-ejlb.onrender.com'
 });
 
 httpServer.interceptors.request.use(
